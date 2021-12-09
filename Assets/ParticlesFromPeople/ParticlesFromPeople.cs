e690c5905d2450960c24193228fb1766021429c8
tai5863 <60917493+tai5863@users.noreply.github.com>
2021-12-09 18:28:55 +0900

    [update] 実機テスト

--- Assets/ParticlesFromPeople/ParticlesFromPeople.cs
+++ Assets/ParticlesFromPeople/ParticlesFromPeople.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
-using UnityEngine; 
+using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.XR.ARFoundation; 
-using UnityEngine.VFX; 
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.VFX;
 
 [RequireComponent(typeof(Camera))]
-[RequireComponent(typeof(ARCameraBackground))] [RequireComponent(typeof(AROcclusionManager))] public class ParticlesFromPeople : MonoBehaviour
+[RequireComponent(typeof(ARCameraBackground))]
+[RequireComponent(typeof(AROcclusionManager))]
+public class ParticlesFromPeople : MonoBehaviour
 {
     struct ThreadSize
     {
@@ -43,26 +45,30 @@ using UnityEngine.VFX;
 
     void OnEnable()
     {
-        // VFXグラフのインスタンスを作成
+        // Get Instance of VFX  Set Up Capture Texture
         m_VFXInstance = Instantiate(m_VFXPrefab, transform.parent);
         m_CaptureTexture = new RenderTexture(Screen.width, Screen.height, 16);
         m_CaptureTexture.Create();
 
-        // コンピュートシェーダのセットアップ
         SetupComputeShader();
     }
 
-    void onDisabe() {
+    void onDisabe()
+    {
+        // Destroy Instance of VFX
         Destroy(m_VFXInstance);
         m_CaptureTexture.Release();
         m_PositionTexture.Release();
     }
 
-    void Update() {
+    void Update()
+    {
+        // Get Human Textures
         Texture2D stencilTexture = m_AROcclusionManager.humanStencilTexture;
         Texture2D depthTexture = m_AROcclusionManager.humanDepthTexture;
 
-        if (stencilTexture == null || depthTexture == null) {
+        if (stencilTexture == null || depthTexture == null)
+        {
             return;
         }
 
@@ -73,7 +79,30 @@ using UnityEngine.VFX;
         m_ComputeShader.SetMatrix("ProjectionMatrix", m_Camera.projectionMatrix);
         m_ComputeShader.Dispatch(m_Kernel, Mathf.CeilToInt(m_PositionTexture.width / m_ThreadSize.x), Mathf.CeilToInt(Screen.height / m_ThreadSize.y), 1);
 
-        m_VFXInstance.SetTexture("Color Map", stencilTexture);
+        m_VFXInstance.SetTexture("Color Map", m_CaptureTexture);
+        m_VFXInstance.SetTexture("Stencil Map", stencilTexture);
+    }
+
+    void SetupComputeShader()
+    {
+        m_PositionTexture = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat);
+        m_PositionTexture.enableRandomWrite = true;
+        m_PositionTexture.Create();
+
+        m_Kernel = m_ComputeShader.FindKernel("GeneratePositionTexture");
+        uint threadSizeX, threadSizeY, threadSizeZ;
+        m_ComputeShader.GetKernelThreadGroupSizes(m_Kernel, out threadSizeX, out threadSizeY, out threadSizeZ);
+        m_ThreadSize = new ThreadSize(threadSizeX, threadSizeY, threadSizeZ);
+
+        m_ComputeShader.SetTexture(m_Kernel, "PositionTexture", m_PositionTexture);
         m_VFXInstance.SetTexture("Position Map", m_PositionTexture);
+    }
+
+    void LateUpdate()
+    {
+        if (m_ARCameraBackground.material != null)
+        {
+            Graphics.Blit(null, m_CaptureTexture, m_ARCameraBackground.material);
+        }
     }
 }
